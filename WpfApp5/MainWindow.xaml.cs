e67dcaa0190939c57845470cefcc9aae5a387d1a
LeSzofer <79213428+LeSzofer@users.noreply.github.com>
2023-07-07 21:37:19 +0200

    problemy z gitem

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -12,35 +12,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Markup; // Wymagane dla klonowania stron
 
 namespace WpfApp5
 {
     public partial class MainWindow : Window
     {
+        private Page1 page1;
+        private Page2 page2;
+        private Page3 page3;
+        private Page4 page4;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Zainicjuj strony tylko raz
+            page1 = new Page1();
+            page2 = new Page2();
+            page3 = new Page3();
+            page4 = new Page4();
+
             NavigateToPage1();
         }
 
         private void NavigateToPage1()
         {
-            MainFrame.Navigate(new Page1());
+            MainFrame.Navigate(page1);
         }
 
         private void NavigateToPage2()
         {
-            MainFrame.Navigate(new Page2());
+            MainFrame.Navigate(page2);
         }
 
         private void NavigateToPage3()
         {
-            MainFrame.Navigate(new Page3());
+            MainFrame.Navigate(page3);
         }
 
         private void NavigateToPage4()
         {
-            MainFrame.Navigate(new Page4());
+            MainFrame.Navigate(page4);
         }
 
         private void ButtonPage1_Click(object sender, RoutedEventArgs e)
@@ -62,5 +75,33 @@ namespace WpfApp5
         {
             NavigateToPage4();
         }
+
+        private void OpenCurrentInNewWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Page newPage = null;
+            if (MainFrame.Content == page1) newPage = new Page1();
+            else if (MainFrame.Content == page2) newPage = new Page2();
+            else if (MainFrame.Content == page3) newPage = new Page3();
+            else if (MainFrame.Content == page4) newPage = new Page4();
+
+            if (newPage != null)
+            {
+                NewWindow newWindow = new NewWindow(newPage);
+                newWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Nie udało się otworzyć strony w nowym oknie.");
+            }
+        }
     }
 }
+public class NewWindow : Window
+    {
+        public NewWindow(Page page)
+        {
+            // Ustaw content okna na przekazaną stronę
+            this.Content = page;
+        }
+    }
+
--- WpfApp5/Page4.xaml.cs
+++ WpfApp5/Page4.xaml.cs
@@ -15,14 +15,100 @@ using System.Windows.Shapes;
 
 namespace WpfApp5
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Page4.xaml
-    /// </summary>
     public partial class Page4 : Page
     {
+        private ProjektEntities db;
+
         public Page4()
         {
             InitializeComponent();
+            db = new ProjektEntities();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            DataGridWypozyczenia.ItemsSource = db.Wypozyczenia.ToList();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Wypozyczenia newWypozyczenie = new Wypozyczenia
+            {
+                IDKlienta = int.Parse(TextBoxIDKlienta.Text),
+                ID_Samochodu = int.Parse(TextBoxIDSamochodu.Text),
+                KodPracownika = int.Parse(TextBoxKodPracownika.Text),
+                Data_Wypozyczenia = DatePickerDataWypozyczenia.SelectedDate.GetValueOrDefault(),
+                Data_Zwrotu = DatePickerDataZwrotu.SelectedDate.GetValueOrDefault()
+            };
+
+            db.Wypozyczenia.Add(newWypozyczenie);
+            db.SaveChanges();
+
+            LoadData();
+            ClearFields();
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGridWypozyczenia.SelectedItem == null)
+            {
+                MessageBox.Show("Wybierz wypożyczenie do edycji.");
+                return;
+            }
+
+            Wypozyczenia selectedWypozyczenie = (Wypozyczenia)DataGridWypozyczenia.SelectedItem;
+
+            selectedWypozyczenie.IDKlienta = int.Parse(TextBoxIDKlienta.Text);
+            selectedWypozyczenie.ID_Samochodu = int.Parse(TextBoxIDSamochodu.Text);
+            selectedWypozyczenie.KodPracownika = int.Parse(TextBoxKodPracownika.Text);
+            selectedWypozyczenie.Data_Wypozyczenia = DatePickerDataWypozyczenia.SelectedDate.GetValueOrDefault();
+            selectedWypozyczenie.Data_Zwrotu = DatePickerDataZwrotu.SelectedDate.GetValueOrDefault();
+
+            db.SaveChanges();
+
+            LoadData();
+            ClearFields();
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGridWypozyczenia.SelectedItem == null)
+            {
+                MessageBox.Show("Wybierz wypożyczenie do usunięcia.");
+                return;
+            }
+
+            Wypozyczenia selectedWypozyczenie = (Wypozyczenia)DataGridWypozyczenia.SelectedItem;
+
+            db.Wypozyczenia.Remove(selectedWypozyczenie);
+            db.SaveChanges();
+
+            LoadData();
+            ClearFields();
+        }
+
+        private void DataGridWypozyczenia_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataGridWypozyczenia.SelectedItem != null)
+            {
+                Wypozyczenia selectedWypozyczenie = (Wypozyczenia)DataGridWypozyczenia.SelectedItem;
+
+                TextBoxIDKlienta.Text = selectedWypozyczenie.IDKlienta.ToString();
+                TextBoxIDSamochodu.Text = selectedWypozyczenie.ID_Samochodu.ToString();
+                TextBoxKodPracownika.Text = selectedWypozyczenie.KodPracownika.ToString();
+                DatePickerDataWypozyczenia.SelectedDate = selectedWypozyczenie.Data_Wypozyczenia;
+                DatePickerDataZwrotu.SelectedDate = selectedWypozyczenie.Data_Zwrotu;
+            }
+        }
+
+        private void ClearFields()
+        {
+            TextBoxIDKlienta.Text = "";
+            TextBoxIDSamochodu.Text = "";
+            TextBoxKodPracownika.Text = "";
+            DatePickerDataWypozyczenia.SelectedDate = null;
+            DatePickerDataZwrotu.SelectedDate = null;
         }
     }
 }
